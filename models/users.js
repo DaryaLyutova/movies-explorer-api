24f770cd4ac7a79bd0ef3630334ee9c074e31d43
Darya Lutuva <lutowa.darya@yandex.ru>
2021-02-09 22:28:24 +0300

    feat: сщоздание схем и моделей user и movie

---
+++ models/users.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => { return isEmail(v); },
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      // нашёлся — сравниваем хеши
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user; // теперь user доступен
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
